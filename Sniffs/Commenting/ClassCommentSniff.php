784a2088210d05d1e71cf918967fd01b06f28e89
Alexandru G <alex@gentle.ro>
2014-08-02 18:17:30 +0300

    @license tag is optional in file and class

---
+++ Sniffs/Commenting/ClassCommentSniff.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Parses and verifies the doc comments for classes.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006-2012 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * {@inheritDoc}
+ *
+ * Changes:
+ *  1. @license tag is optional
+ */
+class eOS_Sniffs_Commenting_ClassCommentSniff extends PEAR_Sniffs_Commenting_FileCommentSniff
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected $tags = array(
+        'category'   => array(
+            'required'       => true,
+            'allow_multiple' => false,
+            'order_text'     => 'precedes @package',
+        ),
+        'package'    => array(
+            'required'       => true,
+            'allow_multiple' => false,
+            'order_text'     => 'follows @category',
+        ),
+        'subpackage' => array(
+            'required'       => false,
+            'allow_multiple' => false,
+            'order_text'     => 'follows @package',
+        ),
+        'author'     => array(
+            'required'       => true,
+            'allow_multiple' => true,
+            'order_text'     => 'follows @subpackage (if used) or @package',
+        ),
+        'copyright'  => array(
+            'required'       => false,
+            'allow_multiple' => true,
+            'order_text'     => 'follows @author',
+        ),
+        'license'    => array(
+            'required'       => false,
+            'allow_multiple' => false,
+            'order_text'     => 'follows @copyright (if used) or @author',
+        ),
+        'version'    => array(
+            'required'       => false,
+            'allow_multiple' => false,
+            'order_text'     => 'follows @license',
+        ),
+        'link'       => array(
+            'required'       => true,
+            'allow_multiple' => true,
+            'order_text'     => 'follows @version',
+        ),
+        'see'        => array(
+            'required'       => false,
+            'allow_multiple' => true,
+            'order_text'     => 'follows @link',
+        ),
+        'since'      => array(
+            'required'       => false,
+            'allow_multiple' => false,
+            'order_text'     => 'follows @see (if used) or @link',
+        ),
+        'deprecated' => array(
+            'required'       => false,
+            'allow_multiple' => false,
+            'order_text'     => 'follows @since (if used) or @see (if used) or @link',
+        ),
+    );
+}
